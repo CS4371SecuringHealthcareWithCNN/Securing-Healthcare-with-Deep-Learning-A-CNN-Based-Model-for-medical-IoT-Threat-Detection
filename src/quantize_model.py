92bcb4e30d1c4bee39de45aac828ac64f4d24669
mam1181 <mam1181@txstate.edu>
2026-04-27 21:07:56 -0500

    enabled float16 DRQ

--- src/quantize_model.py
+++ src/quantize_model.py
@@ -4,11 +4,26 @@ import numpy as np
 def quantize_model(model):
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    return converter.convert()
+    converter.target_spec.supported_types = [tf.float16]
+    qmodel = converter.convert()
+    with open("model.tflite", "wb") as f:
+        f.write(qmodel)
+    return qmodel
 
 def quantize_model_predict(tflite_model_bytes, X_sample):
     interpreter = tf.lite.Interpreter(model_content=tflite_model_bytes)
     interpreter.allocate_tensors()
+
+    for d in interpreter.get_tensor_details():
+        try:
+            tensor = interpreter.get_tensor(d["index"])
+        except ValueError:
+            continue  # skip non-materialized tensors
+
+        if tensor.ndim < 2:
+            continue  # skip scalars
+
+        print(f"{d['name']:50s} | {tensor.dtype} | {tensor.nbytes} bytes")
 
     input_details  = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
